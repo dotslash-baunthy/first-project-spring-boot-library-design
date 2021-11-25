a185636384f85d5e9b0676c83ae56e352d92dc01
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-11-25 22:55:25 +0530

    Indent

--- src/main/java/com/akshitbaunthiyal/library/SpringBootLibraryDesignApplication.java
+++ src/main/java/com/akshitbaunthiyal/library/SpringBootLibraryDesignApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootLibraryDesignApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootLibraryDesignApplication.class, args);
-		System.out.println("Hello spring boot!");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLibraryDesignApplication.class, args);
+        System.out.println("Hello spring boot!");
+    }
 
 }
