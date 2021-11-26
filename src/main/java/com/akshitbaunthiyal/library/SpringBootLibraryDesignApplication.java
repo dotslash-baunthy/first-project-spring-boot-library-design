6d0b46756bd5f613ca5e589277e2272e819d4c74
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-11-26 21:45:04 +0530

    Refactor
    Removed comments

--- src/main/java/com/akshitbaunthiyal/library/SpringBootLibraryDesignApplication.java
+++ src/main/java/com/akshitbaunthiyal/library/SpringBootLibraryDesignApplication.java
@@ -1,6 +1,5 @@
 package com.akshitbaunthiyal.library;
 
-//import com.akshitbaunthiyal.library.model.AkshitBaunthiyal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,18 +9,10 @@ public class SpringBootLibraryDesignApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootLibraryDesignApplication.class, args);
-//        System.out.println("Hello spring boot!");
     }
 
     @Override
     public void run(String... args) throws Exception {
-//
-////        AkshitBaunthiyal akshitBaunthiyal = new AkshitBaunthiyal();
-////
-////        akshitBaunthiyal.setCourseName("Spring with Java");
-////        akshitBaunthiyal.setCourseType("Information technology");
-////        akshitBaunthiyal.setInstructorName("Samarth Narula");
-////
-////        System.out.println(akshitBaunthiyal.toString());
+//        Runs before main()
     }
 }
