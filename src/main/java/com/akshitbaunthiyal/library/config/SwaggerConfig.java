c9fa7ce500f7c8ecf19ac678d00d494389712b0f
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-11-26 22:43:29 +0530

    Update SwaggerConfig.java
    Customized API info

--- src/main/java/com/akshitbaunthiyal/library/config/SwaggerConfig.java
+++ src/main/java/com/akshitbaunthiyal/library/config/SwaggerConfig.java
@@ -2,7 +2,10 @@ package com.akshitbaunthiyal.library.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,8 +15,19 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
     @Bean
     public Docket libraryApi() {
-        return new Docket(DocumentationType.SWAGGER_2).groupName("Library-API")
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).groupName("Library-API")
                 .select().apis(RequestHandlerSelectors.basePackage("com.akshitbaunthiyal.library.controller"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Library API")
+                .description("Library-API reference for developers")
+                .termsOfServiceUrl("https://fakelibrary.com")
+                .contact(new Contact("Fake Library", "https://fakelibrary.com", "akshit.baunthy@gmail.com"))
+                .license("Library License")
+                .licenseUrl("https://fakelibrary.com")
+                .version("1.0")
                 .build();
     }
 }
