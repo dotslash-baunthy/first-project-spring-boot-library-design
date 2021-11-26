bb7660e6ec6d3ea97a68f42c38e3842fe97558fa
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-11-26 10:06:34 +0530

    CommandLineRunner implemented

--- src/main/java/com/akshitbaunthiyal/library/SpringBootLibraryDesignApplication.java
+++ src/main/java/com/akshitbaunthiyal/library/SpringBootLibraryDesignApplication.java
@@ -1,20 +1,20 @@
 package com.akshitbaunthiyal.library;
 
 //import com.akshitbaunthiyal.library.model.AkshitBaunthiyal;
-//import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootLibraryDesignApplication /* implements CommandLineRunner */ {
+public class SpringBootLibraryDesignApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootLibraryDesignApplication.class, args);
 //        System.out.println("Hello spring boot!");
     }
 
-//    @Override
-//    public void run(String... args) throws Exception {
+    @Override
+    public void run(String... args) throws Exception {
 //
 ////        AkshitBaunthiyal akshitBaunthiyal = new AkshitBaunthiyal();
 ////
@@ -23,5 +23,5 @@ public class SpringBootLibraryDesignApplication /* implements CommandLineRunner 
 ////        akshitBaunthiyal.setInstructorName("Samarth Narula");
 ////
 ////        System.out.println(akshitBaunthiyal.toString());
-//    }
+    }
 }
