655010d873bdd1389d187dd8228d70bf86315277
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-11-27 08:51:57 +0530

    Called Course constructor in CommandLineRunner

--- src/main/java/com/akshitbaunthiyal/library/SpringBootLibraryDesignApplication.java
+++ src/main/java/com/akshitbaunthiyal/library/SpringBootLibraryDesignApplication.java
@@ -1,5 +1,6 @@
 package com.akshitbaunthiyal.library;
 
+import com.akshitbaunthiyal.library.model.Course;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,5 +15,7 @@ public class SpringBootLibraryDesignApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 //        Runs before main()
+        Course course = new Course("Designing microservices with Spring Boot", "Information technology", "Samarth Narula");
+        System.out.println("Course - " + course);
     }
 }
