1a39005107c6a7728e97b482116b022214f40b20
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-11-28 07:36:01 +0530

    Update SpringBootLibraryDesignApplication.java
    Instruct Java to can entities under the package com.akshitbaunthiyal.library.entity

--- src/main/java/com/akshitbaunthiyal/library/SpringBootLibraryDesignApplication.java
+++ src/main/java/com/akshitbaunthiyal/library/SpringBootLibraryDesignApplication.java
@@ -6,8 +6,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.akshitbaunthiyal.library.entity")
 @Slf4j
 public class SpringBootLibraryDesignApplication implements CommandLineRunner {
 
