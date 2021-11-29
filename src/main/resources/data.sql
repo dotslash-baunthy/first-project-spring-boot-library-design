489d6733e8afeaede0a4212c892ab8714a2e88e0
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-11-29 08:13:53 +0530

    Added few dummy records
    H2 is a volatile database that needs some entries. The data is inserted through data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO LIBRARY
+VALUES (1, 'Learn Java, Learn Scala, Learn AWS', 'Technology Library');
+INSERT INTO LIBRARY
+VALUES (2, 'Mars: Red Planet, Titan: With Water', 'Space Library');
+INSERT INTO LIBRARY
+VALUES (3, '', 'Blank Library 1');
+INSERT INTO LIBRARY
+VALUES (4, '', 'Blank Library 2');
