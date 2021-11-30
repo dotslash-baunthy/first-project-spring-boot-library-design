d6ca9cbdc7bf45caff28bda519b6a1749e00680c
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-11-30 20:54:10 +0530

    Update SpringBootLibraryDesignApplication.java
    Call functions created in LibraryReadServiceImpl

--- src/main/java/com/akshitbaunthiyal/library/SpringBootLibraryDesignApplication.java
+++ src/main/java/com/akshitbaunthiyal/library/SpringBootLibraryDesignApplication.java
@@ -1,5 +1,6 @@
 package com.akshitbaunthiyal.library;
 
+import com.akshitbaunthiyal.library.entity.Library;
 import com.akshitbaunthiyal.library.model.Course;
 import com.akshitbaunthiyal.library.model.FullName;
 import com.akshitbaunthiyal.library.service.LibraryReadServiceImpl;
@@ -9,6 +10,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EntityScan("com.akshitbaunthiyal.library.entity")
@@ -28,5 +34,9 @@ public class SpringBootLibraryDesignApplication implements CommandLineRunner {
 //        Runs before main()
         log.info("Fetch all libraries -> {}" + libraryReadService.getAllLibrary());
         log.info("Fetch all libraries with no books -> {}" + libraryReadService.getAllLibrariesWithNoBooks());
+        log.info("Fetch page of the library -> {}" + libraryReadService.getLibrariesPaged(2, 2).get().collect(Collectors.toList()));
+        log.info("Fetch inserted records in set order -> {}" + libraryReadService.getInsertWithIdInOrder(Sort.Direction.DESC));
+        log.info("--------------------");
+        log.info("Fetch libraries fetched, sorted and given books -> {}" + libraryReadService.getLibrariesPagedAndSortedByNameAndWithTheseBooks("").get().collect(Collectors.toList()));
     }
 }
