9897fc4fc6b1462d1e07df6fec6a46edc4ff00fe
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-12-01 17:37:48 +0530

    Update SpringBootLibraryDesignApplication.java
    Removed un-needed imports, updated LibraryReadServiceImpl.getInsertWithIdInOrder() such that it does not pass an argument

--- src/main/java/com/akshitbaunthiyal/library/SpringBootLibraryDesignApplication.java
+++ src/main/java/com/akshitbaunthiyal/library/SpringBootLibraryDesignApplication.java
@@ -1,23 +1,15 @@
 package com.akshitbaunthiyal.library;
 
-import com.akshitbaunthiyal.library.entity.Library;
-import com.akshitbaunthiyal.library.model.Course;
-import com.akshitbaunthiyal.library.model.FullName;
 import com.akshitbaunthiyal.library.service.LibraryReadServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Sort;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
-@EntityScan("com.akshitbaunthiyal.library.entity")
 @Slf4j
 public class SpringBootLibraryDesignApplication implements CommandLineRunner {
 
@@ -35,7 +27,7 @@ public class SpringBootLibraryDesignApplication implements CommandLineRunner {
         log.info("Fetch all libraries -> {}" + libraryReadService.getAllLibrary());
         log.info("Fetch all libraries with no books -> {}" + libraryReadService.getAllLibrariesWithNoBooks());
         log.info("Fetch page of the library -> {}" + libraryReadService.getLibrariesPaged(2, 2).get().collect(Collectors.toList()));
-        log.info("Fetch inserted records in set order -> {}" + libraryReadService.getInsertWithIdInOrder(Sort.Direction.DESC));
+        log.info("Fetch inserted records in set order -> {}" + libraryReadService.getInsertWithIdInOrder());
         log.info("--------------------");
         log.info("Fetch libraries fetched, sorted and given books -> {}" + libraryReadService.getLibrariesPagedAndSortedByNameAndWithTheseBooks("").get().collect(Collectors.toList()));
     }
