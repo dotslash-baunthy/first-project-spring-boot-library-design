2cf411d087bc511372ea8b30cb635c55c2d001b2
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-12-02 08:11:03 +0530

    Create LibraryCreateServiceImpl.java

---
+++ src/main/java/com/akshitbaunthiyal/library/service/LibraryCreateServiceImpl.java
@@ -0,0 +1,18 @@
+package com.akshitbaunthiyal.library.service;
+
+import com.akshitbaunthiyal.library.entity.Library;
+import com.akshitbaunthiyal.library.repository.LibraryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LibraryCreateServiceImpl {
+    @Autowired
+    LibraryRepository createRepository;
+
+    public String addSingleLibrary(Library library) {
+        createRepository.save(library);
+        createRepository.flush();
+        return "Library saved";
+    }
+}
