2b2a47cb3d51c466d57c165321dd3b0c623a3984
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-12-02 08:10:56 +0530

    Change id type from int to Long
    find() methods expect a variable of Long type

--- src/main/java/com/akshitbaunthiyal/library/entity/Library.java
+++ src/main/java/com/akshitbaunthiyal/library/entity/Library.java
@@ -16,7 +16,7 @@ public class Library {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
     private String name;
     private String commaSeparatedBookNames;
 }
