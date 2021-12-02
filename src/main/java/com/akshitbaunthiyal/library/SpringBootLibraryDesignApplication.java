f3c3927b53e9f99fa991ef809e347626ba71e68a
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-12-02 08:13:36 +0530

    Update SpringBootLibraryDesignApplication.java
    Extracted all read methods into readServiceImplMethodsExecution() for cleaner code, commented it out for a clean result

--- src/main/java/com/akshitbaunthiyal/library/SpringBootLibraryDesignApplication.java
+++ src/main/java/com/akshitbaunthiyal/library/SpringBootLibraryDesignApplication.java
@@ -1,5 +1,6 @@
 package com.akshitbaunthiyal.library;
 
+import com.akshitbaunthiyal.library.entity.Library;
 import com.akshitbaunthiyal.library.service.LibraryReadServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
@@ -26,18 +28,54 @@ public class SpringBootLibraryDesignApplication implements CommandLineRunner {
 
     public void run(String... args) throws Exception {
 //        Runs before main()
-        log.info("Fetch all libraries -> {}" + libraryReadService.getAllLibrary());
-        log.info("Fetch all libraries with no books -> {}" + libraryReadService.getAllLibrariesWithNoBooks());
-        log.info("Fetch page of the library -> {}" + libraryReadService.getLibrariesPaged(2, 2).get().collect(Collectors.toList()));
-        log.info("Fetch inserted records in set order -> {}" + libraryReadService.getInsertWithIdInOrder());
+//        readServiceImplMethodsExecution();
+    }
+
+    private void readServiceImplMethodsExecution() {
         log.info("--------------------");
-        log.info("Fetch libraries fetched, sorted and given books -> {}" + libraryReadService.getLibrariesPagedAndSortedByNameAndWithTheseBooks("").get().collect(Collectors.toList()));
+        log.info("Fetch all libraries -> {}", libraryReadService.getAllLibrary());
         log.info("--------------------");
 
-//        This needs to be corrected soon!
-//        List<Long> ids = new ArrayList<Long>();
-//        ids.add(1l);
-//        ids.add(2l);
-//        log.info("Fetch libraries by given IDs -> {}"+libraryReadService.getLibrariesById(ids));
+        log.info("--------------------");
+        log.info("Fetch all libraries with no books -> {}", libraryReadService.getAllLibrariesWithNoBooks());
+        log.info("--------------------");
+
+        log.info("--------------------");
+        log.info("Fetch page of the library -> {}", libraryReadService.getLibrariesPaged(2, 2).get().collect(Collectors.toList()));
+        log.info("--------------------");
+
+        log.info("--------------------");
+        log.info("Fetch inserted records in set order -> {}", libraryReadService.getInsertWithIdInOrder());
+        log.info("--------------------");
+
+        log.info("--------------------");
+        log.info("Fetch libraries fetched, sorted and given books -> {}", libraryReadService.getLibrariesPagedAndSortedByNameAndWithTheseBooks("").get().collect(Collectors.toList()));
+        log.info("--------------------");
+
+        log.info("--------------------");
+        List<Long> ids = new ArrayList<Long>();
+        ids.add(1l);
+        ids.add(2l);
+        log.info("Fetch libraries by given IDs -> {}", libraryReadService.getLibrariesById(ids));
+        log.info("--------------------");
+
+        log.info("--------------------");
+        Optional<Library> optionalLibrary = libraryReadService.getLibraryByID(1l);
+        if (optionalLibrary.isPresent()) {
+            log.info("Fetch library by ID if exists -> {}", optionalLibrary);
+        } else {
+            log.info("Given ID does not exist in database");
+        }
+        log.info("--------------------");
+
+        log.info("--------------------");
+        optionalLibrary = libraryReadService.getLibraryWithTheseBooks("Momma Ka purse, Doodh Ki Vyatha: Chai banoon ya bachde ki khurakh, Teen Pair Vaala Kutta");
+        if(optionalLibrary.isPresent()) {
+            log.info("Fetch library with given books -> {}", optionalLibrary);
+        }
+        else {
+            log.info("Library with given book(s) does not exist");
+        }
+        log.info("--------------------");
     }
 }
