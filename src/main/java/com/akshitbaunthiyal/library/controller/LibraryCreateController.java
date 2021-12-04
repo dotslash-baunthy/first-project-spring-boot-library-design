5bf38c8e8601bd4922941466bcdfbf2de0438651
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-12-04 16:42:39 +0530

    Update LibraryCreateController.java
    Removed unused import

--- src/main/java/com/akshitbaunthiyal/library/controller/LibraryCreateController.java
+++ src/main/java/com/akshitbaunthiyal/library/controller/LibraryCreateController.java
@@ -2,7 +2,6 @@ package com.akshitbaunthiyal.library.controller;
 
 import com.akshitbaunthiyal.library.entity.Library;
 import com.akshitbaunthiyal.library.service.LibraryCreateService;
-import com.akshitbaunthiyal.library.service.LibraryCreateServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
