1fa7ea8eb0d892c7d58123d851cca60647b60315
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-12-04 17:45:21 +0530

    Update Library.java
    Removed meta import, added individual imports (probably lighter)

--- src/main/java/com/akshitbaunthiyal/library/entity/Library.java
+++ src/main/java/com/akshitbaunthiyal/library/entity/Library.java
@@ -5,13 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+
 public class Library {
 
     @Id
