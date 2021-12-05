f16d8dc3aa1262981bbb0de9426e7265ef7681ce
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-12-05 06:56:57 +0530

    Update LibraryReadController.java
    Get library by ID or Name or both.

--- src/main/java/com/akshitbaunthiyal/library/controller/LibraryReadController.java
+++ src/main/java/com/akshitbaunthiyal/library/controller/LibraryReadController.java
@@ -4,10 +4,12 @@ import com.akshitbaunthiyal.library.entity.Library;
 import com.akshitbaunthiyal.library.service.LibraryReadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/read")
@@ -20,6 +22,28 @@ public class LibraryReadController {
     @GetMapping("/libraries")
     public List<Library> getAllLibraries() {
         return libraryReadService.getAllLibrary();
+    }
+
+    @GetMapping("/library/")
+    public Optional<Library> getLibraryById(Long id, String name) {
+        if (id != null && name == null) {
+            return libraryReadService.getLibraryByID(id);
+        } else if (name != null && id == null) {
+            return libraryReadService.getLibraryByName(name);
+        } else if (id != null && name != null) {
+            Optional<Library> libraryById = libraryReadService.getLibraryByID(id);
+            if (libraryById.isPresent()) {
+                if (libraryById.get().getName() == libraryReadService.getLibraryByName(name).get().getName()) {
+                    return libraryById;
+                } else {
+                    return null;
+                }
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
     }
 
 }
