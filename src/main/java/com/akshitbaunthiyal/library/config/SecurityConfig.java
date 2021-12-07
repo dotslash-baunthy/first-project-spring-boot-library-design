e8821848e16b635f6393542bc1ad7e5b0bdec479
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-12-07 14:06:49 +0530

    Create SecurityConfig.java

---
+++ src/main/java/com/akshitbaunthiyal/library/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.akshitbaunthiyal.library.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("akshit").password("Captain_007").roles("LIBRARY_OWNER");
+    }
+
+    @Bean
+    PasswordEncoder getPasswordEncode() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
